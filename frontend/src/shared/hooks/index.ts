a0300daa4039780a9304596c2d6f569ed7f51d74
Jarvis Chen <jarvis@soez.tw>
2026-04-21 21:09:06 +0800

    refactor: remove unused file management hooks and components; add tests for template file sections

--- frontend/src/shared/hooks/index.ts
+++ frontend/src/shared/hooks/index.ts
@@ -3,7 +3,6 @@
  */
 
 export { useContainerImages } from './useContainerImages';
-export { useFileTreeSelection } from './useFileTreeSelection';
 export { useFormState, type UseFormStateReturn } from './useFormState';
 export { useFormValidation, type UseFormValidationReturn, type ValidationRules } from './useFormValidation';
 export { useI18n } from './useI18n';
